bb1aad75bf6c6b47b3c4025b6377e9e8111bfc21
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-08-04 19:16:05 +0300

    Webpack bundle is working

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: './style.css'
+        })
+    ],
+    devServer: {
+        port: 8888,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties'
+                        ]
+                    }
+                }
+            },
+            {
+            test: /\.css/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(eot|ttf|woff)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
+}
